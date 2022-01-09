90aed33085d178d397e3e0934e26e2a74ce90c02
rock1357 <riccardosciarra88@gmail.com>
2022-01-09 14:09:22 +0100

    made a simple function with a for loop!

--- temp.py
+++ temp.py
@@ -5,5 +5,13 @@ Spyder Editor
 This is a temporary script file.
 """
 
-a="hello world"
-a
+a=["A","B","C"]
+def function (a):
+    if a[1]>"0":
+        for b in a:
+            print(b)
+            
+    else:
+        return 1
+        
+    return 
