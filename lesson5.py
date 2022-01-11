8a6ce92e76a71640da69e787f948e8e784b7bb33
rock1357 <riccardosciarra88@gmail.com>
2022-01-11 17:35:27 +0100

    lesson 5 about "logging and debugging" code; we defined a fuunction that should return the values of a list (numeric) but it ends with an error: "list index out of range". We must fix the bug but we do not know whaere the mistake line is.

---
+++ lesson5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 17:31:17 2022
+
+@author: riccardosciarra
+"""
+
+
+
+dati = [1, 2, 3, 4]
+
+def my_function():
+    for dato in dati:
+        print(dati[dato])
+        
+my_function()
