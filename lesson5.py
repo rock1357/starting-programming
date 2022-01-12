ac354a8981d4374a4474e162038fbd4d5cf49c01
rock1357 <riccardosciarra88@gmail.com>
2022-01-12 16:24:51 +0100

    il precedente, ignoto, problema è stato trovato e corretto, grazie all'uso del debugger ipdb invocato ad aiutarci nello scansionare linea per linea il nostro programma. L'istruzione per invocarlo è: %run -d <nome_del_file.py> . Utile risuta anche l'utilizzo della funzione: breakdown().

--- lesson5.py
+++ lesson5.py
@@ -12,6 +12,28 @@ dati = [1, 2, 3, 4]
 
 def my_function():
     for dato in dati:
+        
         print(dati[dato])
+           
+    
+        
+my_function()
+
+#%%
+
+"the error is fixed: dato comincia da dato=1 mentre l'array ha dati[0] che non verrà visualizzato"
+" perciò l'ultimo numero che visualizziamo: dati(dato=4) non esiste."
+"Per trovare l'errore è risultato utile usare l'ipython debugger che viene invocato tramite il comando '%run -d nomefile.py' "
+
+
+
+dati = [1, 2, 3, 4]
+
+def my_function():
+    for dato in dati:
+        
+        print(dati[dato-1])
+           
+    
         
 my_function()
